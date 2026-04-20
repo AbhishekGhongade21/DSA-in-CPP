33edef8515ef334f9e0444633b989c9a89372faf
AbhishekGhongade21 <abhishekvg2016@gmail.com>
2026-04-20 13:51:14 +0530

    DSA DAY 2

---
+++ Simple If-Else (Age Check).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+
+    return 0;
+}
